afe4178d212c6444827e9d3a7a1ead22b58ace98
React <3809164@gmail.com>
2024-01-10 12:04:13 +0200

    did it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import Calendar from 'react-calendar'
 
 function App() {
+    const [date, setDate] = useState(new Date())
+    const onChange = date => {
+        setDate(date)
+    }
+
     return (
-        <div>
-            afsdf111
+        <div className={"w-full"}>
+            <div className={"m-auto flex flex-col justify-center text-center max-w-[300px]"}>
+                <Calendar
+                    className={"border-2"}
+                    onChange={onChange}
+                    value={date}
+                />
+                <div className={"text-center py-7 text-red-700"}>
+                    {date.toDateString()}
+                </div>
+            </div>
         </div>
     );
 }
